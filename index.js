650d73ded6b990d92e84ea3bae44d5d64b296dc5
Igor Szyporyn Jørgensen <isj@Igors-MacBook-Pro.local>
2019-03-13 20:05:42 +0100

    fixed dist index filenames

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-'use strict'
+"use strict";
 
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./dist/formalizer-core.cjs.production.js')
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./dist/core.cjs.production.js");
 } else {
-  module.exports = require('./dist/formalizer-core.cjs.development.js')
+  module.exports = require("./dist/core.cjs.development.js");
 }
